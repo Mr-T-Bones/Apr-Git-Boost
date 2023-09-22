bb8ecfb9702c231afb6d0418ca95aa5f0c436508
Mr-T-Bones <alejeli2005@hotmail.com>
2023-09-21 21:36:51 -0600

    Proyecto Terminado XD

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const modalImagen = document.querySelector('#modal-imagen');
 
+if(modalImagen)
+{
+    
 modalImagen.addEventListener('show.bs.modal',function(event){
     const enlace = event.relatedTarget;
     const rutaImagen = enlace.getAttribute('data-bs-imagen');
@@ -15,8 +18,28 @@ const contenidoModal = document.querySelector('.modal-body');
     contenidoModal.appendChild(imagen);
   
 
-})
+});
 modalImagen.addEventListener('hidden.bs.modal',function(){
     const contenidoModal = document.querySelector('.modal-body');
     contenidoModal.textContent = '';
-})
+});
+}
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
